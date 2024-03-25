4b3efde12c4a6d65d42a330a6aaaa66ac2f33f8c
Adam Cipkala <acipkala@gmail.com>
2024-03-25 23:02:01 +0100

    authcontroller

---
+++ UserService/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using UserService.Model;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly AuthService _authService;
+
+    public AuthController(AuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(string username, string password)
+    {
+    
+        User userToCreate = new User
+        {
+            Username = username
+        };
+
+        var createdUser = await _authService.Register(userToCreate, password);
+        return StatusCode(201, createdUser);
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login(string username, string password)
+    {
+        var user = await _authService.Login(username, password);
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid username or password.");
+        }
+
+        return Ok("Login successful!");
+    }
+}
